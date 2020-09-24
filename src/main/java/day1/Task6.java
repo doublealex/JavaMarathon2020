44357c8ae72710b46f4f6154fb9f9094fd2214da
Kazakov Alexander <doublealex92@yandex.ru>
2020-09-24 21:36:05 +0500

    Add Day1 Task solutions

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int n = 1;
+        int k = 9;
+        while (n != 10) {
+            System.out.print( n + "*" + k + " = ");
+            System.out.println( n * k);
+            n++;
+        }
+    }
+}
