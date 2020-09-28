767de70be36ed95401db9c03d85f0c9437ca33ed
Kazakov Alexander <doublealex92@yandex.ru>
2020-09-28 23:52:53 +0500

    add day3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,24 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите делимое и делитель");
 
+        for (int i = 0; i <= 5; i++) {
+            if (i == 5) {
+                break;
+            }
+            Double a = scanner.nextDouble();
+            Double b = scanner.nextDouble();
+
+            if (b == 0) {
+                System.out.println("Деление на ноль");
+                continue;
+            }
+            System.out.println(a / b);
+        }
     }
 }
